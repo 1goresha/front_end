a7f2b19d768c913709cf29a91f161a488732a052
1goresha <i.deryabin2017@mail.ru>
2020-09-19 01:15:13 +0300

    add some code

--- hw1/js/test.js
+++ hw1/js/test.js
@@ -1,17 +1,39 @@
-const vehicle = {
-    typeOfMoving: "wheel",
-    doors : 4,
-    color: "black",
+// const vehicle = {
+//     typeOfMoving: "wheel",
+//     doors : 4,
+//     color: "black",
 
-    aboutThisVehicle: function(){
-        console.log(this);
-    }
-};
+//     aboutThisVehicle: function(){
+//         console.log(this);
+//     }
+// };
 
-const sportCar = Object.create(vehicle);
+// const sportCar = Object.create(vehicle);
 
-sportCar.doors = 2;
-sportCar.color = "red";
+// sportCar.doors = 2;
+// sportCar.color = "red";
 
-vehicle.aboutThisVehicle();
-sportCar.aboutThisVehicle();
+// vehicle.aboutThisVehicle();
+// sportCar.aboutThisVehicle();
+
+// let arr = [0, 1, 2, 3, 4];
+// console.log(...arr);
+
+// const obj1 = {
+//     count: 1,
+//     name: "obj1",
+//     size: 3
+// };
+
+// const obj2 = Object.assign({}, obj1);
+// console.log(obj2);
+
+// const obj3 = {...obj1};
+// console.log(obj3);
+
+// const {count, name, size} = obj1;
+// console.log(count, name, size);
+
+const str = prompt("введите список продуктов", "");
+const products = str.split(", ");
+console.log(products.join("; "));
